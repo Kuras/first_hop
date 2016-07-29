60c6f3e828ee372cb7d31b50c4bb07daca4f9853
Przemysław Kura <pkura@pl.sii.eu>
2016-07-29 13:27:10 +0200

    Run spring boot

--- src/main/java/web/JackRabbitRepository.java
+++ src/main/java/web/JackRabbitRepository.java
@@ -4,7 +4,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@ImportResource("classpath:repository.xml")
+@ImportResource("classpath:dmsRepository.xml")
 public class JackRabbitRepository {
+//
+//    @Bean(name = "repository")
+//    public JndiObjectFactoryBean getJndiObjectFactoryBean() {
+//        JndiObjectFactoryBean factoryBean = new JndiObjectFactoryBean();
+//        factoryBean.setJndiName( "java:comp/env/jcr/myRepository" );
+//        return factoryBean;
+//    }
+//
+//    @Bean(name = "jcrSessionFactory")
+//    public JcrSessionFactory getJcrSessionFactory() {
+//        JcrSessionFactory sessionFactory  = new JcrSessionFactory();
+//        return sessionFactory;
+//    }
+//
+//    @Bean(name = "jcrTemplate")
+//    public JcrTemplate getJcrTemplate() {
+//        JcrTemplate jcrTemplate  = new JcrTemplate();
+//        return jcrTemplate;
+//    }
+
 
 }
